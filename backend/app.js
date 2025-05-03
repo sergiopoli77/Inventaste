4a6b8870cada291962aea753a1b5519dfc62d49d
sergiopoli77 <s22210048@student.unklab.ac.id>
2025-05-04 01:58:44 +0800

    import category to app.js

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const { mongoUrl } = require("./config"); // Mengimpor mongoUrl dari config
 const employeeController = require("./app/employees/employee.controller");
 const itemController = require("./app/items/item.controller");
+const categoryController = require("./app/categories/category.controller"); // Mengimpor category controller
 
 const app = express();
 
@@ -25,8 +26,11 @@ app.use(express.static(path.join(__dirname, "public")));
 // Gunakan employee controller untuk API employee
 app.use("/api", employeeController);
 
-// Gunakan employee controller untuk API item
+// Gunakan item controller untuk API item
 app.use("/api", itemController);
+
+// Gunakan category controller untuk API kategori
+app.use("/api", categoryController); // Menambahkan route untuk category
 
 // Koneksi ke MongoDB
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
