ef3e4ca1dd18b29cb4fa32ae8ff2ae8fa2ef948b
sergiopoli77 <s22210048@student.unklab.ac.id>
2025-05-04 01:18:44 +0800

    update item repository

--- backend/app/items/item.repository.js
+++ backend/app/items/item.repository.js
@@ -1,35 +1,30 @@
 const Item = require("./item.model");
 
-// Fungsi untuk mengambil semua item
-const findAll = async () => {
-  return await Item.find();
+const getAllItems = async () => {
+  return await Item.find().populate('id_kategori createdBy updateBy');
 };
 
-// Fungsi untuk mencari item berdasarkan ID
-const findById = async (id) => {
-  return await Item.findById(id);
+const getItemById = async (id) => {
+  return await Item.findById(id).populate('id_kategori createdBy updateBy');
 };
 
-// Fungsi untuk menambah item baru
-const create = async (itemData) => {
-  const item = new Item(itemData);
-  return await item.save();
+const createItem = async (itemData) => {
+  const newItem = new Item(itemData);
+  return await newItem.save();
 };
 
-// Fungsi untuk mengupdate item berdasarkan ID
-const updateById = async (id, itemData) => {
+const updateItem = async (id, itemData) => {
   return await Item.findByIdAndUpdate(id, itemData, { new: true });
 };
 
-// Fungsi untuk menghapus item berdasarkan ID
-const deleteById = async (id) => {
+const deleteItem = async (id) => {
   return await Item.findByIdAndDelete(id);
 };
 
 module.exports = {
-  findAll,
-  findById,
-  create,
-  updateById,
-  deleteById,
+  getAllItems,
+  getItemById,
+  createItem,
+  updateItem,
+  deleteItem
 };
