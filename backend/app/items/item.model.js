b05977280a90a07f80b879350bfc029bf5080a3c
sergiopoli77 <s22210048@student.unklab.ac.id>
2025-05-04 01:18:08 +0800

    add items model

---
+++ backend/app/items/item.model.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+// Schema untuk Item
+const itemSchema = mongoose.Schema(
+  {
+    name: { 
+      type: String, 
+      required: true 
+    },
+    stock: { 
+      type: Number, 
+      required: true, 
+      default: 0 
+    },
+    status: { 
+      type: String, 
+      enum: ["aktif", "non-aktif"], 
+      default: "aktif" 
+    },
+    id_kategori: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: "Category", 
+      required: true 
+    },
+    createdBy: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: "Employee", 
+      required: true 
+    },
+    updateBy: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: "Employee" 
+    }
+  },
+  {
+    versionKey: false,  // Menghapus __v dari schema
+    timestamps: true  // Menambahkan createdAt dan updatedAt
+  }
+);
+
+// Model untuk Item
+const Item = mongoose.model("Item", itemSchema);
+
+module.exports = Item;
